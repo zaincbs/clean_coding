9dccc2be5b5fd507203265972fbd0b9ec88eb651
zaincbs <zaincbs@gmail.com>
2015-05-15 21:18:27 -0700

    Added first project and sample file

---
+++ utils/tic_tac_toe.c
@@ -0,0 +1,51 @@
+/**
+ * @file        sample.c
+ *
+ * @brief       breif line about the program 
+ *
+ * @Author	    Zain Ijaz
+ *
+ * @Occupation	Software Engineer
+ */
+
+/*
+ * These materials are provided under license by UEI.
+ * UEI licenses this file to you under the SOFTWARE EVALUATION LICENSE AGREEMENT
+ * accompanying the SDK delivered to you (the "License").
+ * You may also obtain a copy of the License directly from UEI.
+ * You may not use this file except in strict compliance with the License
+ * and all restrictions set forth therein. Your use and access to the file
+ * constitutes your agreement to be bound by the terms of the License.
+ * Unless required by applicable law or agreed to in writing, all materials
+ * distributed under the License is provided on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, either express or implied.
+ */
+
+#include<stdio.h>
+
+/*
+ ********************************************************************************
+ * Constants.
+ ********************************************************************************
+ */
+
+/*
+ ********************************************************************************
+ * Globals
+ ********************************************************************************
+ */
+
+/*
+ ********************************************************************************
+ * Prototypes.
+ ********************************************************************************
+ */
+
+
+int main(int argc, char argv[])
+{
+
+    printf("Testing file\n");
+
+}
+
